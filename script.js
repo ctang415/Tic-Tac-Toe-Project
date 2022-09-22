c213eab1273d794d027f93c95353a6f5cb55c360
ctang415 <carrietang117@gmail.com>
2022-09-22 18:39:37 -0400

    Set array to update according to player symbol

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 const Gameboard = (() => {
     return {
-        gameBoard: []
+        gameBoard: [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     }
 })();
 
+/*
 const DisplayController = (() => {
     return {
 
     }
 })();
-
+*/
 
 const Player = (name, symbol) => {
-    const makeMove = () => {
-        const squares = document.querySelectorAll('.square');
+    var squares = document.querySelectorAll('.square');
+    const makeMove = () => 
         squares.forEach((square) => { 
         square.addEventListener('click', function() {
-        square.textContent = `$symbol`;
-        console.log("it works")
+        square.textContent = `${symbol}`;
+        Gameboard.gameBoard[square.dataset.id] = `${symbol}`;
+        })
     })
-    });
-    }
-    return {name, symbol, makeMove}
-}
+        return {name, symbol, makeMove}
+};
 
 const playerOne = Player('One', 'X')
 const playerTwo = Player('Two', 'O')
 
-
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => { 
-    square.addEventListener('click', function() {
-    square.textContent = "X";
-    console.log("it works")
-})
-});
+playerTwo.makeMove();
