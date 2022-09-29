a4179fbefe456fa84b66c407561249f676a4d9e4
ctang415 <carrietang117@gmail.com>
2022-09-28 22:25:36 -0400

    Fixed winner's name on game win

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const Gameboard = (() => {
-    const gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    var gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     const checkWin = () => {
         if ((gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[1] != " ") ||
-        (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[4] != " ") || 
-        (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[6] != " ") ||
-        (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[5] != " ") ||
-        (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[4] != " ") ||
-        (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7] && gameBoard[4] != " ") ||
-        (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[4] != " ") ||
-        (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8] && gameBoard[7] != " "))
-            window.alert(`Game is over. ${displayController.currentPlayer.name} wins.`)
-            else if (gameBoard.every(notEmpty)) {
+            (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[4] != " ") || 
+            (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[6] != " ") ||
+            (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[5] != " ") ||
+            (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[4] != " ") ||
+            (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7] && gameBoard[4] != " ") ||
+            (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[4] != " ") ||
+            (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8] && gameBoard[7] != " ")) {
+            displayController.declareWinner();
+            endGame();
+        }
+        else if (gameBoard.every(notEmpty)) {
                 window.alert(`It's a tie.`)
             }
         }
@@ -18,8 +20,37 @@ const Gameboard = (() => {
             return index != " "
         }
     const resetGame = () => {
-        gameBoard = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        for(i=0; i < gameBoard.length; i++) {
+            gameBoard[i] = ' ';
+            playerOne.name = ''
+            playerTwo.name = ''
+            document.getElementById('nameA').style.display = 'block'
+            document.getElementById('nameB').style.display = 'block'
+            document.getElementById('first').textContent = ''
+            document.getElementById('second').textContent = ''
+            document.getElementById('nameA').value = ' '
+            document.getElementById('nameB').value = ' '
+            container.style.visibility = 'hidden'
+        }
+        button.style.display = 'block'
     }
+    const container = document.querySelector('.container')
+    container.style.visibility = 'hidden'
+    const button = document.getElementById('button')
+    button.addEventListener('click', function() {
+    const nameOne = document.getElementById('nameA').value
+    const nameTwo = document.getElementById('nameB').value
+    playerOne.name = nameOne;
+    playerTwo.name = nameTwo;
+    document.getElementById('nameA').style.display = 'none'
+    document.getElementById('nameB').style.display = 'none'
+    document.getElementById('first').textContent = nameOne;
+    document.getElementById('second').textContent = nameTwo;
+    console.log(nameOne)
+    console.log(nameTwo)
+    container.style.visibility = 'visible'
+    button.style.display = 'none'
+})
     return {
         gameBoard, checkWin, resetGame
     }
@@ -31,23 +62,24 @@ const Player = (name, symbol) => {
 };
 
 
-/*
-const button = document.getElementById('button')
+const playerOne = Player('Player One', 'X')
+const playerTwo = Player('Player Two', 'O')
 
-button.addEventListener('click', function() {
-    const nameOne = document.getElementById('nameA')
-    const nameTwo = document.getElementById('nameB')
+const endGame = () => {
+            var squares = document.querySelectorAll('.square')
+            squares.forEach((square) => { 
+            square.removeEventListener('click', function() {
+        })
+    })
+}
 
+const reset = document.getElementById('reset')
+reset.addEventListener('click', function() {
+    Gameboard.resetGame();
 })
 
-        [0, 1, 2], [0, 4, 8], [0, 3, 6], [2, 5, 8], [2, 4, 6],
-        [1, 4, 7], [3, 4, 5], [6, 7, 8]
-*/
-
-
 const displayController = (() => {
-    const playerOne = Player('Player One', 'X')
-    const playerTwo = Player('Player Two', 'O')
+    var counter = 0;
     var currentPlayer = playerOne;
     const makeMove = () => {
             var squares = document.querySelectorAll('.square');
@@ -56,6 +88,7 @@ const displayController = (() => {
                 if(!square.textContent) {
                     square.textContent = currentPlayer.symbol;
                     Gameboard.gameBoard[square.dataset.id] = currentPlayer.symbol;
+                    counter++;
                     console.log('clicked')
                     Gameboard.checkWin();
                 }
@@ -71,16 +104,15 @@ const displayController = (() => {
              currentPlayer = playerOne;
         }
     }
-    const endGame = () => {
-        if (Gameboard.checkWin()) {
-            var squares = document.querySelectorAll('.square')
-            squares.forEach((square) => { 
-                square.removeEventListener('click', function() {
-        })
-    })
-}
+    const declareWinner = () => {
+        if(counter%2 != 0) {
+            return  window.alert(`${playerOne.name} is the winner.`)
+        }
+        else {
+            return window.alert(`${playerTwo.name} is the winner.`)
+        }
     }
-            return {makeMove, switchPlayer, currentPlayer, endGame}
+            return {makeMove, switchPlayer, declareWinner}
 })();
 
 
@@ -103,3 +135,5 @@ const Gameflow = (() => {
 })();
 
 Gameflow.play();
+
+
